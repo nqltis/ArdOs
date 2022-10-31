702ab61e1767782211e4526e0b62db56c7db4cc6
nqltis <nathan.simonf1@gmail.com>
2022-10-31 15:24:09 +0100

    Imported readFileName() and nextFile() and created _strCopy

--- ArdOs.ino
+++ ArdOs.ino
@@ -17,41 +17,73 @@ LCDoutput lcdoutput(RS, RW, E, DataPin);
 
 
 #define fsz 0 //dummy for futur implementation of file size
+#define empt 0 //dummy for empty mem
 
-int mempos = 0;
-char memory[128] = {
-  1, '/', fsz, fsz, 0, 23, 4, 'h', 
-  'o', 'm', 'e', fsz, fsz, 0, 10, 0, 
-  6, 's', 'y', 's', 't', 'e', 'm', fsz, 
-  fsz, 0, 20, 0, 0
+int mempos = 8;
+char memory[] ={
+  129, '/', fsz, fsz, 0, 6, 0, 50,            //0   /{
+  131, 'b', 'l', 'g', fsz, fsz, 0, 16,        //8   blg(
+  0, 0, 6, 'L', 'O', '.', 't', 'x',           //16  ), syste
+  't', fsz, fsz, 0, 29, 0, 0, 5,              //24  m{>  },
+  'a', '.', 't', 'x', 't', fsz, fsz, 0,       //32  a.txt
+  41, 0, 48, 'h', 'e', 'l', 'l', 'o',         //40  (hello
+  0, 59, 0, 57,
+                empt, empt, empt, empt,       //48   
+  empt, 0, 123, 0, 0, 131, 'u', 's',          //56  
+  'r', fsz, fsz, 0, 69, 0, 121, 5,            //64
+  'm', 'y', 'l', 'u', 'a', fsz, fsz, 0,       //72
+  81, 0, 91, '!', '#', 'l', 'u', 'a',         //80
+  '5', '.', '3', 0, 0, 6, 'm', 'a',           //88
+  'i', 'n', '.', 'c', fsz, fsz, 0, 104,       //96
+  0, 120, 'i', 'n', 't', ' ', 'm', 'a',       //104
+  'i', 'n', '(' ,')', ' ', '{', '}', 0,       //112 endof>
+  0, 0, 0, 0, 0                               //120 >file - endofusr - endofroot
 };
+int history[32] = {0};
+byte histPtr = 0;
 
-//nsz, n,a,m,e, fsz, fsz, eob, eob, {ctnt...}, 0
+//{blg{}, LO.txt(), a.txt(hello), usr{a.lua(!#lua5.3), main.c(int main() {})}}
 
-String readFileName(int address) {
-  String str = "";
-  int endOfName = address + memory[address];
-  for (int i = address + 1; i <= endOfName; i++) {
-    str = str + memory[i]; 
+//nsz + 128(dir), n,a,m,e, fsz, fsz, red, red
+//eoChunkaddr (2B), {content}, red, red 
+//eob, eob
+
+void _strCopy(char *str1, char *str2) { //copy str1 to str2
+  int i = 0;
+  while (str1[i]) {
+    str2[i] = str1[i];
+    i++;
   }
-  return str;
+  str2[i] = 0;
+  return;
+}
+void readFileName(char *str, int address) {  //input char[] and file address. 
+                                              //write in char[] name of file at address.
+  int sizeOfName = memory[address] & 127; //get rid of dir flag
+  for (int i = 1; i <= sizeOfName; i++) {
+    str[i - 1] = memory[address + i]; 
+  }
+  str[sizeOfName] = 0;
+  return;
 }
 int nextFile(int address) {
-  int endOfBlock;
-  int addr;
+  int addr = 0;
+  int nextaddr = address;
+  int nsize = memory[nextaddr] & 127; //get rid of dir flag 
+  nextaddr += nsize + 3;    //address of eob
+  //  Start of +  Size of name + skip
+  //    file                   file size
   do {
-    addr = address + memory[address] + 3; // address of first byte of eob
-    //        Start of +  Size of name + skip
-    //          file                   file size
-    endOfBlock = (memory[addr] << 8) | memory[addr + 1];
-  } while (endOfBlock);
+    addr = nextaddr;
+    nextaddr = (memory[nextaddr] << 8) | memory[nextaddr + 1]; //read address of endofblock
+  } while (nextaddr);
   return (addr + 2);
 }
 
-String buff1 = "";
-String buff2 = "";
+char buff1[16] = "";
+char buff2[16] = "";
 void setup() {
-  buff2 = readFileName(mempos);
+  readFileName(buff2, mempos);
   lcdoutput.init();
   lcdoutput.printScreen(buff1, buff2);
 }
@@ -61,8 +93,8 @@ void loop() {
     case 'B':
       mempos = nextFile(mempos);
       if (memory[mempos]) {
-        buff1 = buff2;
-        buff2 = readFileName(mempos);
+        _strCopy(buff2, buff1);
+        readFileName(buff2, mempos);
         lcdoutput.printScreen(buff1, buff2);
       }
     break;
