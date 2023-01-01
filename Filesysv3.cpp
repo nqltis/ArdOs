6916602e83b505c7cc94175ad711c9d0cd49cf16
nqltis <nathan.simonf1@gmail.com>
2023-01-01 19:09:09 +0100

    Implemented most reading functions of Filesysv3

--- Filesysv3.cpp
+++ Filesysv3.cpp
@@ -5,6 +5,82 @@
   nqltis - 31/12/2022
 */
 
-//#include <EEPROM.h>
+#include <EEPROM.h>
 #include "Filesysv3.h"
 
+File::File() {  //Create Invalid File
+  headerPtr = 255;
+}
+
+File::File(HEADER_ID_TYPE _headerPtr) {
+  headerPtr = _headerPtr;
+}
+
+void File::initfs(int offset) {
+  ABS_ADDR_TYPE _blockAddr = getFreeBlock();
+  unsigned char rootBlock[4] = {0, 0, '/', 0};  //Write file name in block
+  for (char i = 0; i < 4; i++) {
+    EEPROM.update(_blockAddr + i, rootBlock[i]);
+  }
+  unsigned char rootHeader[5] = {0, 0, 0, 0, 0}; //Create header for root dir
+  rootHeader[0] = _blockAddr;
+  for (char i = 0; i < 5; i++) EEPROM.update(offset + i, rootHeader[i]);
+}
+
+char File::isDir() {
+  return EEPROM.read(headerPtr) & 128;
+}
+char File::isExecutable() {
+  return EEPROM.read(headerPtr) & 64;
+}
+
+void File::getName(char *output) {
+  unsigned int offset = getFirstBlock() + 2*BLOCK_ID_SIZE;
+  for (unsigned char i = 0; i < BLOCK_SIZE - 2*BLOCK_ID_SIZE; i++) {
+    output[i] = EEPROM.read(i + offset);
+    if (output[i] == 0) break;
+  }
+}
+
+void File::getParent() {
+  unsigned char ptrOffset = BLOCK_ID_SIZE;
+  HEADER_ID_TYPE targetAddr = EEPROM.read(headerPtr + ptrOffset);
+  if (targetAddr != 0) headerPtr = targetAddr;
+}
+void File::getChild() {
+  unsigned char ptrOffset = BLOCK_ID_SIZE + HEADER_ID_SIZE;
+  HEADER_ID_TYPE targetAddr = EEPROM.read(headerPtr + ptrOffset);
+  if (targetAddr != 0) headerPtr = targetAddr;
+}
+void File::getPrev() {
+  unsigned char ptrOffset = BLOCK_ID_SIZE + 2*HEADER_ID_SIZE;
+  HEADER_ID_TYPE targetAddr = EEPROM.read(headerPtr + ptrOffset);
+  if (targetAddr != 0) headerPtr = targetAddr;
+}
+void File::getNext() {
+  unsigned char ptrOffset = BLOCK_ID_SIZE + 3*HEADER_ID_SIZE;
+  HEADER_ID_TYPE targetAddr = EEPROM.read(headerPtr + ptrOffset);
+  if (targetAddr != 0) headerPtr = targetAddr;
+}
+
+BLOCK_ID_TYPE File::getFirstBlock() {
+  return BLOCK_AREA_OFFSET + BLOCK_SIZE * EEPROM.read(headerPtr);
+}
+BLOCK_ID_TYPE File::getFreeBlock() {
+  for (ABS_ADDR_TYPE i = 0; i < BLOCK_AREA_OFFSET - BLOCK_MAP_OFFSET; i++) {
+    unsigned char _byte = EEPROM.read(i + BLOCK_MAP_OFFSET);
+    for (char j = 0; j < 8; j++) {
+      if (!(_byte & (1 << j))) return i * 8 + j;
+    }
+  } 
+}
+void File::reserveBlock(BLOCK_ID_TYPE blockId) {
+  ABS_ADDR_TYPE targetByteAddr = blockId/8 + BLOCK_MAP_OFFSET;
+  unsigned char targetByte = EEPROM.read(targetByteAddr);
+  targetByte = targetByte | (1 << (blockId % 8));
+}
+void File::freeBlock(BLOCK_ID_TYPE blockId) {
+  ABS_ADDR_TYPE targetByteAddr = blockId/8 + BLOCK_MAP_OFFSET;
+  unsigned char targetByte = EEPROM.read(targetByteAddr);
+  targetByte = targetByte & ~(1 << (blockId % 8));
+}
--- Filesysv3.h
+++ Filesysv3.h
@@ -8,36 +8,43 @@
 #ifndef CAO_FILE_SYSTEM_V3
 #define CAO_FILE_SYSTEM_V3
 
-//#include <EEPROM.h>
+#include <EEPROM.h>
 
 #define BLOCK_SIZE 16
 #define BLOCK_ID_TYPE unsigned char
 #define BLOCK_ID_SIZE 1   //Number of bytes of BLOCK_ID_TYPE
+#define INDEX_TYPE char
 #define BLOCK_MAP_OFFSET 160  //Address of block map
 #define BLOCK_AREA_OFFSET 224  //Address of first data block
+#define MEMORY_SIZE 1024
 #define HEADER_ID_TYPE unsigned char 
 #define HEADER_ID_SIZE 1
+#define ABS_ADDR_TYPE unsigned int//Size of absolute address (1024B -> int)
 
 class File
 {
   public:
     File();
     File(HEADER_ID_TYPE _headerPtr);
-    void initfs();
+    void initfs(int offset);
     char isDir();
     char isExecutable();
     void getName(char *output);
-    void getNext();
-    void getPrev();
-    void getChild();
     void getParent();
+    void getChild();
+    void getPrev();
+    void getNext();
     void open();
     char dataRemaining();
     unsigned char read();
   private:
-    unsigned char headerPtr;
-    unsigned char block;  //Set when open
+    HEADER_ID_TYPE headerPtr;
+    BLOCK_ID_TYPE block;  //Set when open
     unsigned char index;  //Set when open
+    BLOCK_ID_TYPE getFirstBlock();
+    BLOCK_ID_TYPE getFreeBlock();
+    void reserveBlock(BLOCK_ID_TYPE blockId);
+    void freeBlock(BLOCK_ID_TYPE blockId);
 };
 
 #endif
