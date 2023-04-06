c673582807774ebc6518d855475282b5129dc4c6
nqltis <nathan.simonf1@gmail.com>
2023-04-06 02:35:37 +0200

    Modified M16input to use a 74HC138 demultiplexer

--- M16input.cpp
+++ M16input.cpp
@@ -7,15 +7,19 @@
 #include <Arduino.h>
 #include "M16input.h"
 
-M16input::M16input(const byte InputPins[8]) {
-  for (byte i = 0; i < 8; i++) { //Save pins in a private array
+M16input::M16input(const byte OutputPins[2], const byte InputPins[4]) {
+  for (byte i = 0; i < 4; i++) { //Save pins in a private array
     _inputpins[i] = InputPins[i];
   }
-  for (byte i = 0; i < 4; i++) {
-    pinMode(_inputpins[i], OUTPUT);
-    pinMode(_inputpins[i + 4], INPUT_PULLUP);
-    digitalWrite(_inputpins[i], HIGH);
+  _outputpins[0] = OutputPins[0];
+  _outputpins[1] = OutputPins[1];
+  for (byte i = 0; i < 4; i++) {  //Set pins mode
+    pinMode(_inputpins[i], INPUT_PULLUP);
   }
+  pinMode(_outputpins[0], OUTPUT);
+  pinMode(_outputpins[1], OUTPUT);
+  digitalWrite(_outputpins[0], LOW);
+  digitalWrite(_outputpins[1], LOW);
 }
 
 /* 4 MSB are outputs, 4 LSB are inputs.
@@ -25,21 +29,21 @@ connection on the pin's wire */
 byte M16input::inRead() { 
   byte input = 0;
   for (byte i = 0; i < 4; i++) {  //for each output
-    digitalWrite(_inputpins[i], LOW);
+    digitalWrite(_outputpins[0], i & 1);  //Select demultiplexer output
+    digitalWrite(_outputpins[1], i & 2);
     delay(1);
     for (byte j = 0; j < 4; j++) {  //test every input for connection
-      if (!digitalRead(_inputpins[j + 4])) {
+      if (!digitalRead(_inputpins[j])) {
         input = input | (16 << i) | (1 << j); //OR-write result if connection
       }
     }
-    digitalWrite(_inputpins[i], HIGH);
   }
   return input;
 }
 
 char M16input::translate(byte inbyte) { //Link inRead() output to corresponding key
   switch (inbyte) {
-    case 0: return ' ';
+    case 0: return ' '; //Currently never used
     case 17: return '1';
     case 18: return '2';
     case 20: return '3';
